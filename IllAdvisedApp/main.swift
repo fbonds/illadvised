ee606eabf53afaad7959a232257f5fd363c4052d
Fletcher Bonds <fbonds@gmail.com>
2025-11-06 09:52:37 -0800

    build error changes

--- IllAdvisedApp/main.swift
+++ IllAdvisedApp/main.swift
@@ -1,8 +1,6 @@
 import UIKit
 
-@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
@@ -14,3 +12,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
+
+_ = UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
+
